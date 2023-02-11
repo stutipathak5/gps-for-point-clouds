4cabfebfb27dc9c20c6f898fe77f9ca0c99ce80e
stutipathak5 <stutipathak.97@gmail.com>
2023-02-11 16:14:49 +0100

    updates

--- eval_tester.py
+++ eval_tester.py
@@ -34,8 +34,8 @@ def get_data(file_name):
 
 
 file_name = str(
-    input("Enter file name (exp. bun_zipper.ply or something.csv): ")
-    or "bun_zipper_res3.ply"
+    input("Enter file name (exp. dragon_vrip_0.03): ")
+    or "dragon_vrip_0.03"
 )
 org_coords, org_faces, simp_coords, org_curv = get_data(file_name)
 target_num_points = simp_coords.size(0)
@@ -47,14 +47,19 @@ tcp_simp = top_curvature_simplify(
     org_curv.to(device), org_coords.to(device), target_num_points
 ).to("cpu")
 fps_simp = farthest_point_simplify(org_coords.to(device), target_num_points).to("cpu")
-qdm_simp_coords = qem_simplify(org_coords, org_faces, target_num_points)
+# qdm_simp_coords = qem_simplify(org_coords, org_faces, target_num_points)
+
+np.savetxt("resources/results/" + file_name + "_rand.xyz", rand_simp.numpy(), delimiter=" ")
+np.savetxt("resources/results/" + file_name + "_tcp.xyz", tcp_simp.numpy(), delimiter=" ")
+np.savetxt("resources/results/" + file_name + "_fps.xyz", fps_simp.numpy(), delimiter=" ")
+# np.savetxt("resources/results/" + file_name + "_qdm.xyz", qdm_simp_coords.numpy(), delimiter=" ")
 
 fig = plt.figure(figsize=plt.figaspect(1))
 ax = fig.add_subplot(2, 3, 1, projection="3d")
 ax.set_axis_off()
 ax.scatter(org_coords[:, 0], org_coords[:, 1], org_coords[:, 2], s=1, c=org_curv)
 k = 2
-for i in [simp_coords, rand_simp, tcp_simp, fps_simp, qdm_simp_coords]:
+for i in [simp_coords, rand_simp, tcp_simp, fps_simp]:
     ax = fig.add_subplot(2, 3, k, projection="3d")
     ax.set_axis_off()
     ax.scatter(i[:, 0], i[:, 1], i[:, 2], s=1)
@@ -63,15 +68,15 @@ plt.show()
 
 
 # 2. Compute evaluation metrics
-norm_consis_proposed, chamf_dist_proposed = compute_all_metrics(org_coords, simp_coords)
-norm_consis_rand, chamf_dist_rand = compute_all_metrics(org_coords, rand_simp)
-norm_consis_tcp, chamf_dist_tcp = compute_all_metrics(org_coords, tcp_simp)
-norm_consis_fps, chamf_dist_fps = compute_all_metrics(org_coords, fps_simp)
-norm_consis_qdm, chamf_dist_qdm = compute_all_metrics(org_coords, qdm_simp_coords)
-
-print("Chamfer Distance", "Normals Consistency")
-print("Proposed", chamf_dist_proposed, norm_consis_proposed)
-print("Random", chamf_dist_rand, norm_consis_rand)
-print("TCP", chamf_dist_tcp, norm_consis_tcp)
-print("FPS", chamf_dist_fps, norm_consis_fps)
-print("QDM", chamf_dist_qdm, norm_consis_qdm)
+# norm_consis_proposed, chamf_dist_proposed = compute_all_metrics(org_coords, simp_coords)
+# norm_consis_rand, chamf_dist_rand = compute_all_metrics(org_coords, rand_simp)
+# norm_consis_tcp, chamf_dist_tcp = compute_all_metrics(org_coords, tcp_simp)
+# norm_consis_fps, chamf_dist_fps = compute_all_metrics(org_coords, fps_simp)
+# norm_consis_qdm, chamf_dist_qdm = compute_all_metrics(org_coords, qdm_simp_coords)
+#
+# print("Chamfer Distance", "Normals Consistency")
+# print("Proposed", chamf_dist_proposed, norm_consis_proposed)
+# print("Random", chamf_dist_rand, norm_consis_rand)
+# print("TCP", chamf_dist_tcp, norm_consis_tcp)
+# print("FPS", chamf_dist_fps, norm_consis_fps)
+# print("QDM", chamf_dist_qdm, norm_consis_qdm)
--- gp_point_clouds/data.py
+++ gp_point_clouds/data.py
@@ -1,22 +1,25 @@
 import torch
 import numpy as np
-from jakteristics import compute_features
-from pytorch3d.io import IO
-from pytorch3d.structures.pointclouds import Pointclouds
+import time
 
 
-def get_data(file_name, device="cpu"):
+def get_data_jak(file_name, device="cpu"):
+
+    from pytorch3d.io import IO
+    from pytorch3d.structures.pointclouds import Pointclouds
+    from jakteristics import compute_features
+
     mesh = IO().load_mesh("resources/clouds/" + file_name, device=device)
     coords, faces = mesh.verts_list()[0].double(), mesh.faces_list()[0].double()
     pcd = Pointclouds(points=mesh.verts_list())
-    bounding_box = pcd.get_bounding_boxes().cpu().numpy()
-    bb_min, bb_max = np.array(bounding_box[0, :, 0]), np.array(bounding_box[0, :, 1])
-    diag = bb_max - bb_min
+    bounding_box = pcd.get_bounding_boxes()
+    # bb_min, bb_max = torch.tensor(bounding_box[0,:,0], device=device), torch.tensor(bounding_box[0,:,1], device=device)
+    diag = bounding_box[0,:,1] - bounding_box[0,:,0]
     volume = diag[0] * diag[1] * diag[2]
     surface = volume ** (2 / 3)
     surface_per_point = surface / coords.size(0)
-    radius = np.sqrt(surface_per_point * 12)
-
+    radius = torch.sqrt(surface_per_point * 12)
+    t1=time.time()
     curv = (
         torch.from_numpy(
             compute_features(
@@ -29,5 +32,43 @@ def get_data(file_name, device="cpu"):
         .squeeze(1)
         .to(device)
     )
+    t2 = time.time()
+    print("time jak", t2-t1)
 
     return coords, curv, faces
+
+
+
+def get_data_cc(file_name):
+
+    import cloudComPy as cc
+
+    if file_name.endswith(".ply"):
+        mesh = cc.loadMesh("resources/clouds/" + file_name)
+        faces = mesh.IndexesToNpArray()
+        cloud = mesh.getAssociatedCloud()
+    else:
+        cloud = cc.loadPointCloud("resources/clouds/" + file_name)
+    coords = torch.from_numpy(cloud.toNpArrayCopy()).double()
+    bounding_box = cloud.getOwnBB()
+    bb_min = np.array(bounding_box.minCorner())
+    bb_max = np.array(bounding_box.maxCorner())
+    diag = bb_max - bb_min
+    volume = diag[0] * diag[1] * diag[2]
+    surface = volume ** (2 / 3)
+    surface_per_point = surface / coords.size(0)
+    radius = np.sqrt(surface_per_point * 12)
+    # cc.computeNormals([cloud])
+    t1 = time.time()
+    cc.computeCurvature(
+        cc.CurvatureType.NORMAL_CHANGE_RATE, radius, [cloud]
+    )  # compute curvature as a scalar field
+    t2 = time.time()
+    nsf = cloud.getNumberOfScalarFields()
+    # norm = torch.from_numpy(cloud.getScalarField(nsf - 2).toNpArray()).double()
+    curv = torch.from_numpy(cloud.getScalarField(nsf - 1).toNpArray()).double()
+    print("time cc", t2-t1)
+
+    return coords, curv, faces, volume, radius, surface
+
+
